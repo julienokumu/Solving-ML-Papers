470d79ceeef66d7e405383dce5502991cb011685
julienokumu <codewithjulien@gmail.com>
2025-03-30 11:16:52 +0300

    test 2

--- Deep Neural Networks/AlexNet/alexnet.py
+++ Deep Neural Networks/AlexNet/alexnet.py
@@ -5,42 +5,39 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes=1000):
         super(self, AlexNet).__init__()
         self.conv_layers = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4, padding=0),
+            nn.Conv2d(in_channel=3, out_channel=96, kernel_size=11, stride=4, padding=0),
             nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
+            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75),
             nn.MaxPool2d(kernel_size=3, stride=2),
 
-            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, padding=2),
+            nn.Conv2d(in_channels=96,out_channels=256, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
+            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75),
             nn.MaxPool2d(kernel_size=3, stride=2),
 
-            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True)
         )
-
         self.fc_layers = nn.Sequential(
-            nn.Dropout(p=0.5),
             nn.Linear(in_features=256 * 6 * 6, out_features=4096),
             nn.ReLU(inplace=True),
-
             nn.Dropout(p=0.5),
+
             nn.Linear(in_features=4096, out_features=4096),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
 
             nn.Linear(in_features=4096, out_features=num_classes)
         )
 
     def forward(self, x):
         x = self.conv_layers(x)
-        x = x.view(x.size(0), -1) # flatten and reshape
+        x = x.view(x.size(0), -1)
         x = self.fc_layers(x)
         return x
-    
