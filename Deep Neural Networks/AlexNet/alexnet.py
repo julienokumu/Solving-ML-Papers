06c7d9b36de98dbf063447a77b2f6ce40f6014b8
julienokumu <codewithjulien@gmail.com>
2025-03-30 12:25:20 +0300

    complete alexnet architecture

--- Deep Neural Networks/AlexNet/alexnet.py
+++ Deep Neural Networks/AlexNet/alexnet.py
@@ -4,40 +4,52 @@ import torch.nn as nn
 class AlexNet(nn.Module):
     def __init__(self, num_classes=1000):
         super(self, AlexNet).__init__()
+        # convolutional layers
         self.conv_layers = nn.Sequential(
-            nn.Conv2d(in_channel=3, out_channel=96, kernel_size=11, stride=4, padding=0),
+            # first convolutional layer
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4, padding=0),
             nn.ReLU(inplace=True),
             nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75),
             nn.MaxPool2d(kernel_size=3, stride=2),
 
-            nn.Conv2d(in_channels=96,out_channels=256, kernel_size=3, stride=1, padding=1),
+            # second convolutional layer
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75),
             nn.MaxPool2d(kernel_size=3, stride=2),
 
-            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1),
+            # third convolutional layer
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1),
+            # fourth convolutional layer
+            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1),
-            nn.ReLU(inplace=True)
+            # fifth convolutional layer
+            nn.Conv2d(in_channels=384, out_features=256, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2)
         )
+        # fully connected layers
         self.fc_layers = nn.Sequential(
+            # first fully connected layer
             nn.Linear(in_features=256 * 6 * 6, out_features=4096),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),
 
+            # second fully connected layer
             nn.Linear(in_features=4096, out_features=4096),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),
 
+            # last fully connected layer
             nn.Linear(in_features=4096, out_features=num_classes)
         )
-
+        
     def forward(self, x):
         x = self.conv_layers(x)
         x = x.view(x.size(0), -1)
         x = self.fc_layers(x)
         return x
+    
