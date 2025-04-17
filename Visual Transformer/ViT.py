9d6cdc5f23f10400be5e626a39f4087fd5052e8d
julienokumu <codewithjulien@gmail.com>
2025-04-18 00:45:23 +0300

    positional embedding

--- Visual Transformer/ViT.py
+++ Visual Transformer/ViT.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import math
 
 class PatchEmbedding(nn.Module):
-    def __init__(self, img_size=224, patch_size=16, emb_dim=768, in_channels=3):
+    def __init__(self, img_size=224, patch_size=16, in_channels=3, emb_dim=768):
         super(PatchEmbedding, self).__init__()
         self.img_size = img_size
         self.patch_size = patch_size
@@ -15,9 +15,19 @@ class PatchEmbedding(nn.Module):
             kernel_size=patch_size,
             stride=patch_size
         )
-
+    
     def forward(self, x):
         x = self.projection(x)
         x = x.flatten(2)
         x = x.transpose(1, 2)
-        return x 
+        return x
+    
+class PositionalEmbedding(nn.Module):
+    def __init__(self, num_patches, emb_dim):
+        super(PositionalEmbedding, self).__init__()
+        self.pos_emb = nn.Parameter(torch.zeros(1, num_patches + 1, emb_dim))
+        nn.init.trunc_normal_(self.pos_emb, std=0.02)
+
+    def forward(self, x):
+        x = x + self.pos_emb
+        return x
