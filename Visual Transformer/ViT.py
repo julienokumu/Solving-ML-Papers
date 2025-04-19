83f1210583fa3f00851344911c5c94c21084e382
julienokumu <codewithjulien@gmail.com>
2025-04-20 01:29:40 +0300

    visiontransformer model

--- Visual Transformer/ViT.py
+++ Visual Transformer/ViT.py
@@ -59,3 +59,50 @@ class TransformerEncoder(nn.Module):
         mlp_output = self.mlp(x)
         x = x + mlp_output
         return x 
+    
+class VisionTransformer(nn.Module):
+    def __init__(self, emb_dim=768, mlp_dim=3072, num_classes=1000, dropout=0.1, img_size=224, patch_size=16, in_channels=3, depth=12, num_heads=12):
+        super(VisionTransformer, self).__init__()
+        self.num_patches = (img_size // patch_size)**2
+
+        self.patch_emb = PatchEmbedding(
+            emb_dim,
+            img_size,
+            in_channels,
+            patch_size
+        )
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, emb_dim))
+        nn.init.trunc_normal_(self.cls_token, std=0.02)
+
+        self.pos_emb = PositionalEmbedding(self.num_patches, emb_dim)
+        self.pos_drop = nn.Dropout(dropout)
+
+        self.encoder_layers = nn.ModuleList([
+            TransformerEncoder(
+                emb_dim,
+                num_heads,
+                mlp_dim,
+                dropout
+            )
+            for _ in range(depth)
+        ])
+        self.layer_norm = nn.LayerNorm(emb_dim)
+        self.class_head = nn.Linear(emb_dim, num_classes)
+
+    def forward(self, x):
+        batch_size = x.view(0)
+        x = self.patch_emb(x)
+        cls_token = self.cls_token.expand(batch_size, -1, -1)
+        x = torch.cat((cls_token, x), dim=1)
+        x = x + self.pos_emb(x)
+        x = self.pos_drop(x)
+        for layer in self.encoder_layers:
+            x = layer(x)
+
+        x = self.layer_norm(x)
+        cls_output = x[:, 0]
+        logits = self.class_head(cls_output)
+        return logits
+
+
