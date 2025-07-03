c03143b00ff1e41d5e97ee9d7d862d353ca6920b
julienokumu <codewithjulien@gmail.com>
2025-07-03 21:47:10 +0300

    optimized test

--- DeepSeekMoE/deepseekMoETest.py
+++ DeepSeekMoE/deepseekMoETest.py
@@ -8,40 +8,39 @@ import math
 @dataclass
 class ModelArgs:
     vocab_size: int = 160
-    hidden_dim: int = 320
-    n_layers: int = 2
+    dim: int = 320
+    n_layers: int = 1
     n_heads: int = 5
     head_dim: int = 64
     n_experts: int = 32
     n_shared_experts: int = 1
     n_activated_experts: int = 4
     expert_scale: float = 0.25
-    ffn_inter_scale: int = 2
-    max_seq_len: int = 64
+    ffn_inter_dim: int = 2
     base: float = 10000.0
+    eps: float = 1e-5
+    drop_rate: float = 0.1
 
 args = ModelArgs()
 
 class RMSNorm(nn.Module):
-    def __init__(self, hidden_dim: int, eps: float = 1e-6):
+    def __init__(self, dim: int, eps: float):
         super().__init__()
         self.eps = eps
-        self.gamma = nn.Parameter(torch.ones(hidden_dim))
+        self.gamma = nn.Parameter(torch.ones(dim))
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        batch, seq_len, hidden_dim = x.shape
+        batch, seq_len, dim = x.shape
         rms = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + self.eps)
-        x_norm = x / rms * self.gamma
+        norm_x = x / rms * self.gamma
         print("===RMSNorm Test===")
-        print(f"input shape: {x.shape}, output shape: {x_norm.shape}")
-        return x_norm
+        print(f"input shape: {x.shape}, output shape: {norm_x.shape}")
+        return norm_x
 
 class RoPE(nn.Module):
-    def __init__(self, head_dim: int, base: float):
+    def __init__(self, head_dim: int, theta: torch.Tensor) -> torch.Tensor:
         super().__init__()
         self.head_dim = head_dim
-        self.base = base
-        theta = 1.0 / (base ** (torch.arange(0, head_dim, 2).float() / head_dim))
         self.register_buffer("theta", theta)
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -50,23 +49,25 @@ class RoPE(nn.Module):
         angles = positions * self.theta
         cos = torch.cos(angles).unsqueeze(0).unsqueeze(1)
         sin = torch.sin(angles).unsqueeze(0).unsqueeze(1)
-        x1, x2 = x[..., ::2], x[..., 1::2]
+        x1, x2 = x[..., 0::2], x[..., 1::2]
         qk_rot = torch.cat([-x2 * sin + x1 * cos, x1 * sin + x2 * cos], dim=-1)
         print("===RoPE Test===")
         print(f"input shape: {x.shape}, output shape: {qk_rot.shape}")
         return qk_rot
 
 class ExpertFFN(nn.Module):
-    def __init__(self, hidden_dim: int, inter_dim: int):
+    def __init__(self, dim: int, inter_dim: int, drop_rate: float):
         super().__init__()
-        self.layer1 = nn.Linear(hidden_dim, inter_dim)
+        self.layer1 = nn.Linear(dim, inter_dim)
         self.gelu = nn.GELU()
-        self.layer2 = nn.Linear(inter_dim, hidden_dim)
+        self.dropout = nn.Dropout(drop_rate)
+        self.layer2 = nn.Linear(inter_dim, dim)
     
-    def forward(self, x: torch.Tensor):
-        batch, seq_len, hidden_dim = x.shape
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        batch, seq_len, dim = x.shape
         x = self.layer1(x)
-        output = self.gelu(x)
+        x = self.gelu(x)
+        x = self.dropout(x)
         output = self.layer2(x)
         print("===ExpertFFN Test===")
         print(f"input shape: {x.shape}, output shape: {output.shape}")
@@ -75,30 +76,29 @@ class ExpertFFN(nn.Module):
 class MoE(nn.Module):
     def __init__(self, args):
         super().__init__()
-        standard_inter_dim = int(args.hidden_dim * args.ffn_inter_scale)
+        self.n_shared_experts = args.n_shared_experts
+        self.n_activated_experts = args.n_activated_experts - args.n_shared_experts
+        self.n_routed_experts = args.n_experts - args.n_shared_experts
+        standard_inter_dim = int(args.dim * args.ffn_inter_dim)
         expert_inter_dim = int(standard_inter_dim * args.expert_scale)
         self.shared_experts = nn.ModuleList([
             ExpertFFN(
-                args.hidden_dim,
-                expert_inter_dim
+                args.dim,
+                expert_inter_dim,
+                args.drop_rate
             ) for _ in range(args.n_shared_experts)
         ])
         self.routed_experts = nn.ModuleList([
             ExpertFFN(
-                args.hidden_dim,
-                expert_inter_dim
-            ) for _ in range(args.n_experts - args.n_shared_experts)
+                args.dim,
+                expert_inter_dim,
+                args.drop_rate
+            ) for _ in range(self.n_routed_experts)
         ])
-        self.router = nn.Linear(
-            args.hidden_dim,
-            args.n_experts - args.n_shared_experts
-        )
-        self.n_shared_experts = args.n_shared_experts
-        self.n_activated_experts = args.n_activated_experts - args.n_shared_experts
-        self.n_routed_experts = args.n_experts - args.n_shared_experts
+        self.router = nn.Linear(args.dim, self.n_routed_experts)
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        batch, seq_len, hidden_dim = x.shape
+        batch, seq_len, dim = x.shape
         shared_output = torch.zeros_like(x)
         for expert in self.shared_experts:
             shared_output += expert(x)
@@ -118,61 +118,70 @@ class MoE(nn.Module):
         return output
 
 class MHA(nn.Module):
-    def __init__(self, args):
+    def __init__(self, args, theta: torch.Tensor):
         super().__init__()
-        self.q_proj = nn.Linear(args.hidden_dim, args.n_heads * args.head_dim)
-        self.k_proj = nn.Linear(args.hidden_dim, args.n_heads * args.head_dim)
-        self.v_proj = nn.Linear(args.hidden_dim, args.n_heads * args.head_dim)
-        self.out_proj = nn.Linear(args.n_heads * args.head_dim, args.hidden_dim)
-        self.rope = RoPE(args.head_dim, args.base)
         self.n_heads = args.n_heads
         self.head_dim = args.head_dim
+        self.q_proj = nn.Linear(args.dim, args.n_heads * args.head_dim)
+        self.k_proj = nn.Linear(args.dim, args.n_heads * args.head_dim)
+        self.v_proj = nn.Linear(args.dim, args.n_heads * args.head_dim)
+        self.out_proj = nn.Linear(args.n_heads * args.head_dim, args.dim)
+        self.rope = RoPE(args.head_dim, theta)
         self.scale = 1.0 / math.sqrt(args.head_dim)
+        self.dropout = nn.Dropout(args.drop_rate)
     
-    def forward(self, x: torch.Tensor, posiions: torch.Tensor) -> torch.Tensor:
-        batch, seq_len, hidden_dim = x.shape
+    def forward(self, x: torch.Tensor, positions: torch.Tensor) -> torch.Tensor:
+        batch, seq_len, dim = x.shape
         q = self.q_proj(x).view(batch, seq_len, self.n_heads, self.head_dim).transpose(1, 2)
         k = self.k_proj(x).view(batch, seq_len, self.n_heads, self.head_dim).transpose(1, 2)
         v = self.v_proj(x).view(batch, seq_len, self.n_heads, self.head_dim).transpose(1, 2)
         q = self.rope(q)
         k = self.rope(k)
         scores = torch.matmul(q, k.transpose(-2, -1)) * self.scale
+        mask = torch.triu(torch.ones(seq_len, seq_len, device=x.device), diagonal=1)
+        mask = mask * float('-inf')
+        scores = scores + mask
         attn = F.softmax(scores, dim=-1)
-        context = torch.matmul(attn, v).transpose(1, 2).contiguous().view(batch, seq_len, -1)
-        output = self.out_proj(context)
+        attn = self.dropout(attn)
+        output = torch.matmul(attn, v).transpose(1, 2).contiguous().view(batch, seq_len, -1)
+        output = self.out_proj(output)
         print("===MHA Test===")
         print(f"input shape: {x.shape}, output shape: {output.shape}")
         return output
 
-class TransformerBlock(nn.Module):
-    def __init__(self, args):
+class Block(nn.Module):
+    def __init__(self, args, theta: torch.Tensor):
         super().__init__()
-        self.layer_norm1 = RMSNorm(args.hidden_dim)
-        self.attn = MHA(args)
-        self.layer_norm2 = RMSNorm(args.hidden_dim)
+        self.layer_norm1 = RMSNorm(args.dim, args.eps)
+        self.attn = MHA(args, theta)
+        self.layer_norm2 = RMSNorm(args.dim, args.eps)
         self.moe = MoE(args)
     
-    def forward(self, x: torch.Tensor, positions: torch.Tensor):
-        batch, seq_len, hidden_dim = x.shape
+    def forward(self, x: torch.Tensor, positions: torch.Tensor) -> torch.Tensor:
+        batch, seq_len, dim = x.shape
         x_norm = self.layer_norm1(x)
-        attn_output = self.attn(x_norm, positions) + x
+        attn_output = self.attn(x, positions) + x_norm
         attn_norm = self.layer_norm2(attn_output)
         output = self.moe(attn_norm) + attn_output
-        print("===TransformerBlock Test===")
+        print("===Block Test===")
         print(f"input shape: {x.shape}, output shape: {output.shape}")
         return output
 
 class DeepSeekMoE(nn.Module):
     def __init__(self, args):
         super().__init__()
-        self.emb = nn.Embedding(args.vocab_size, args.hidden_dim)
+        self.emb = nn.Embedding(args.vocab_size, args.dim)
+        theta = 1.0 / (args.base ** (torch.arange(0, args.head_dim, 2).float() / args.head_dim))
+        self.register_buffer("theta", theta)
         self.layers = nn.ModuleList([
-            TransformerBlock(args)
-            for _ in range(args.n_layers)
+            Block(
+                args,
+                theta
+            ) for _ in range(args.n_layers)
         ])
-        self.layer_norm = RMSNorm(args.hidden_dim)
-        self.out_proj = nn.Linear(args.hidden_dim, args.vocab_size)
-
+        self.layer_norm = RMSNorm(args.dim, args.eps)
+        self.out_proj = nn.Linear(args.dim, args.vocab_size)
+    
     def forward(self, x: torch.Tensor, positions: Optional[torch.Tensor] = None) -> torch.Tensor:
         batch, seq_len = x.shape
         if positions is None:
@@ -183,11 +192,11 @@ class DeepSeekMoE(nn.Module):
             x = layer(x, positions)
         x = self.layer_norm(x)
         logits = self.out_proj(x)
-        print("===DeepSeekMoE Test===")
+        print("===DeepSeekMoE 2B Test===")
         print(f"input shape: {x.shape}, output shape: {logits.shape}")
         return logits
 
-if  __name__ == "__main__":
+if __name__ == "__main__":
     torch.manual_seed(42)
     batch, seq_len = 2, 16
     x = torch.randint(0, args.vocab_size, (batch, seq_len))
